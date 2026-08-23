17fbd8db811ad8382c56b4071f79ef112364384d
mythmota <motabernardo9@gmail.com>
2026-08-23 23:05:22 +0100

    Add MainActivity with overlay permission handling
    Implement overlay permission check and service start logic.

---
+++ app/src/main/java/com/chesshelper/myapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.chesshelper.myapp
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        checkOverlayPermission()
+    }
+
+    private fun checkOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                val intent = Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:$packageName")
+                )
+                startActivityForResult(intent, 1234)
+            } else {
+                startOverlayService()
+            }
+        } else {
+            startOverlayService()
+        }
+    }
+
+    private fun startOverlayService() {
+        val intent = Intent(this, OverlayService::class.java)
+        startService(intent)
+        finish()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1234) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(this)) {
+                startOverlayService()
+            }
+        }
+    }
+}
