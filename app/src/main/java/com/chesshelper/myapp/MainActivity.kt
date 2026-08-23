d856b502383b2bbee9450fa0f2d9d9c264bdf77e
mythmota <motabernardo9@gmail.com>
2026-08-24 00:23:00 +0100

    Update MainActivity.kt

--- app/src/main/java/com/chesshelper/myapp/MainActivity.kt
+++ app/src/main/java/com/chesshelper/myapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.chesshelper.myapp
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.media.projection.MediaProjectionManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -8,39 +11,52 @@ import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val OVERLAY_PERMISSION_REQ_CODE = 1234
+    private val SCREEN_CAPTURE_REQ_CODE = 5678
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         checkOverlayPermission()
     }
 
     private fun checkOverlayPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (!Settings.canDrawOverlays(this)) {
-                val intent = Intent(
-                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:$packageName")
-                )
-                startActivityForResult(intent, 1234)
-            } else {
-                startOverlayService()
-            }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE)
         } else {
-            startOverlayService()
+            requestScreenCapturePermission()
         }
     }
 
-    private fun startOverlayService() {
-        val intent = Intent(this, OverlayService::class.java)
-        startService(intent)
-        finish()
+    private fun requestScreenCapturePermission() {
+        val projectionManager = getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+        startActivityForResult(projectionManager.createScreenCaptureIntent(), SCREEN_CAPTURE_REQ_CODE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1234) {
+
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(this)) {
-                startOverlayService()
+                requestScreenCapturePermission()
             }
+        } else if (requestCode == SCREEN_CAPTURE_REQ_CODE) {
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                val serviceIntent = Intent(this, OverlayService::class.java).apply {
+                    putExtra("RESULT_CODE", resultCode)
+                    putExtra("DATA_INTENT", data)
+                }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForegroundService(serviceIntent)
+                } else {
+                    startService(serviceIntent)
+                }
+            }
+            finish()
         }
     }
 }
